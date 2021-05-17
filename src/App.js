bc6227213da6cd5e46ae4dfaf28d7e31564d953e
Gabriel Henton <gabehenton@gmail.com>
2021-05-16 18:18:43 -0700

    login works and takes you to protected route the bubbles page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import BubblePage from './components/BubblePage';
+import { axiosWithAuth } from './helpers/axiosWithAuth';
+import { PrivateRoute } from './helpers/PrivateRoute';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import './styles.scss';
 
 function App() {
+  // const logout = () => {
+  //   axiosWithAuth.post('http://localhost:5000/api/logout').then((res) => {
+  //     console.log('Logged out!');
+  //     localStorage.removeItem('token');
+  //     window.location.href = '/';
+  //   });
+  // };
+
   return (
     <Router>
-      <div className="App">
+      <div className='App'>
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
-        </header> 
+          <a data-testid='logoutButton' href='#'>
+            <Link to='/login'>Login</Link>
+          </a>
+        </header>
 
-        <Route exact path="/" component={Login} />
+        <Switch>
+          <PrivateRoute exact path='/bubblepage' component={BubblePage} />;
+          <Route exact path='/' component={Login} />
+        </Switch>
       </div>
     </Router>
   );
