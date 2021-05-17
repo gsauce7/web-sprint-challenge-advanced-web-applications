b8050f88be21d913921f54a102cd7b641fad9074
Gabriel Henton <gabehenton@gmail.com>
2021-05-16 20:16:14 -0700

    MVP complete with all tests passing

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import BubblePage from './components/BubblePage';
-import { axiosWithAuth } from './helpers/axiosWithAuth';
 import { PrivateRoute } from './helpers/PrivateRoute';
-
 import Login from './components/Login';
 import './styles.scss';
 
 function App() {
-  // const logout = () => {
-  //   axiosWithAuth.post('http://localhost:5000/api/logout').then((res) => {
-  //     console.log('Logged out!');
-  //     localStorage.removeItem('token');
-  //     window.location.href = '/';
-  //   });
-  // };
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+  };
 
   return (
     <Router>
       <div className='App'>
         <header>
           Color Picker Sprint Challenge
-          <a data-testid='logoutButton' href='#'>
-            <Link to='/login'>Login</Link>
+          <a onClick={handleLogout} data-testid='logoutButton' href='/'>
+            logout
           </a>
         </header>
-
         <Switch>
-          <PrivateRoute exact path='/bubblepage' component={BubblePage} />;
           <Route exact path='/' component={Login} />
+          <PrivateRoute exact path='/bubblepage' component={BubblePage} />;
         </Switch>
       </div>
     </Router>
